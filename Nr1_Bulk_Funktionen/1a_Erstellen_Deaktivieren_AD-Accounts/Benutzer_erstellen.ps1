1964992c37c68a4df03bacd9a0ec1bee08381219
21u6624 <21u6624@bztf.ch>
2023-05-18 14:39:29 +0200

    funktioniert noch nicht

--- Nr1_Bulk_Funktionen/1a_Erstellen_Deaktivieren_AD-Accounts/Benutzer_erstellen.ps1
+++ Nr1_Bulk_Funktionen/1a_Erstellen_Deaktivieren_AD-Accounts/Benutzer_erstellen.ps1
@@ -1,1 +1,42 @@
 Write-Host "Benutzer erstellen"
+
+
+function Import_Benutzer {
+    
+        $path = Read-Host -Prompt "Pfadangabe ohne xml endung"
+        $path1 = $path
+        $path1+=".xml" 
+        $path2 = $path
+        $path2 += ".csv"
+        $import = Import-Clixml -path ($path1) -WarningAction Stop | out-file -Path ($path2) 
+
+       (Get-Content $path2 | Select-Object -Skip 1) | Set-Content $path2
+    $import = Import-Csv -Path ($path2) -Delimiter ";" 
+  Remove-Item -Path ($path2) 
+$import | erstellen 
+
+}
+
+function erstellen {
+   
+    process{
+        if ($_.name -like "*-*") {
+            
+        }
+        else {
+        
+        
+       write $_.Name
+        [String]$display = ($_.Vorname)
+        $display += " "
+        $display += ($_.Name)
+        New-ADUser -name $_.Benutzername -path AD:\"OU=Schueler, DC=powershell,DC=dreck" 
+        Set-ADUser  -UserPrincipalName ($_.Benutzername) -DisplayName ($display) -Enabled $true -GivenName ($_.Vorname) -Surname ($_.Name) -PasswordNotRequired $true
+    }}
+    end{
+        Get-ADUser -Filter *
+    }
+
+    
+}
+Import_Benutzer
